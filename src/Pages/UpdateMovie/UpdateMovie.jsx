81811d17b70dd5d72d17b2b44453fc8f35f4a204
ADDER 08 <arunchrisstan@gmail.com>
2022-06-07 21:02:39 +0530

    Form

--- src/Pages/UpdateMovie/UpdateMovie.jsx
+++ src/Pages/UpdateMovie/UpdateMovie.jsx
@@ -10,106 +10,121 @@ import { useState } from "react";
 import axios from "../../axios";
 import UploadService from "../../services/uploadService";
 import movieCrudService, {
-    get_movie_by_id,
+  get_movie_by_id,
 } from "../../services/movieCrudService";
+import Select from "react-select";
+import { optionLength } from "./data";
+
 
 function UpdateMovie() {
-    const [movie, setMovie] = useState("");
-    const [movieId, setMovieId] = useState("");
-    const [movieTitle, setMovieTitle] = useState("");
-    const [movieName, setMovieName] = useState("");
-    const [movieDescp, setMovieDescp] = useState("");
-    const [release, setRelease] = useState("");
-    const [popularity, setPopularity] = useState("");
-    const [budget, setBudget] = useState("");
-    const [revenue, setRevenue] = useState("");
-    const [poster, setPoster] = useState("");
-    const [backDrop, setBackDrop] = useState("");
-    const [media, setMedia] = useState("");
-    const [message, setMessage] = useState("");
-    const [successful, setSuccessful] = useState(false);
-    const { id } = useParams();
+  const [movie, setMovie] = useState("");
+  const [movieId, setMovieId] = useState("");
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieName, setMovieName] = useState("");
+  const [movieDescp, setMovieDescp] = useState("");
+  const [release, setRelease] = useState("");
+  const [popularity, setPopularity] = useState("");
+  const [budget, setBudget] = useState("");
+  const [revenue, setRevenue] = useState("");
+  const [poster, setPoster] = useState("");
+  const [backDrop, setBackDrop] = useState("");
+  const [media, setMedia] = useState("");
+  const [message, setMessage] = useState("");
+  const [successful, setSuccessful] = useState(false);
+  const [datas, setDatas] = useState([]);
+  const { id } = useParams();
+  console.log("!!!!!!!1",datas);
+  const data = async () => {
+    const res = await axios
+      .get(`/movies/get_movie_by_id/${id}`)
+      .then((response) => {
+        setMovieId(response.data.movie_id);
+        setMovieTitle(response.data.movie_title);
+        setMovieName(response.data.movie_name);
+        setMovieDescp(response.data.movie_description);
+        setRelease(response.data.m_release_date);
+        setPopularity(response.data.m_popularity);
+        setBudget(response.data.movie_budget);
+        setRevenue(response.data.m_revenue);
+        setPoster(response.data.posterPath);
+        setBackDrop(response.data.backdrop_path);
+        setMedia(response.data.media_path);
+        console.log("👉👉 >>", response.data);
+      })
+      .catch((error) => {
+        // console.log(error.res);
+      });
+  };
 
-    const data = async () => {
-        const res = await axios
-            .get(`/movies/get_movie_by_id/${id}`)
-            .then((response) => {
-                setMovieId(response.data.movie_id);
-                setMovieTitle(response.data.movie_title);
-                setMovieName(response.data.movie_name);
-                setMovieDescp(response.data.movie_description);
-                setRelease(response.data.m_release_date);
-                setPopularity(response.data.m_popularity);
-                setBudget(response.data.movie_budget);
-                setRevenue(response.data.m_revenue);
-                setPoster(response.data.posterPath);
-                setBackDrop(response.data.backdrop_path);
-                setMedia(response.data.media_path);
-                console.log("👉👉 >>", response.data);
-            })
-            .catch((error) => {
-                // console.log(error.res);
-            });
-    };
+  useEffect(() => {
+    data();
+  }, []);
 
-    useEffect(() => {
-        data();
-    }, []);
+  const navToListPage = useNavigate();
 
-    const navToListPage = useNavigate();
+  const required = (value) => {
+    if (!value) {
+      setMessage("Plz Fill Out all the required Feilds");
+      return (
+        <div className="failure_msg">
+          <sup>*</sup>This field is required!
+        </div>
+      );
+    }
+  };
 
-    const required = (value) => {
-        if (!value) {
-            setMessage("Plz Fill Out all the required Feilds");
-            return (
-                <div className="failure_msg">
-                    <sup>*</sup>This field is required!
-                </div>
-            );
+  const submit = (e) => {
+    e.preventDefault();
+    setMessage("");
+    setSuccessful(false);
+    movieCrudService
+      .update_movie(
+        movieId,
+        movieTitle,
+        movieName,
+        movieDescp,
+        release,
+        popularity,
+        budget,
+        revenue,
+        poster,
+        backDrop,
+        media
+      )
+      .then(
+        (response) => {
+          setMessage(response.data.message);
+          setSuccessful(true);
+          navToListPage("/movies");
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+          setMessage(resMessage);
+          setSuccessful(false);
         }
-    };
+      );
+    console.log("message ", message);
+  };
 
-    const submit = (e) => {
-        e.preventDefault();
-        setMessage("");
-        setSuccessful(false);
-        movieCrudService
-            .update_movie(
-                movieId,
-                movieTitle,
-                movieName,
-                movieDescp,
-                release,
-                popularity,
-                budget,
-                revenue,
-                poster,
-                backDrop,
-                media
-            )
-            .then(
-                (response) => {
-                    setMessage(response.data.message);
-                    setSuccessful(true);
-                    navToListPage("/movies");
-                },
-                (error) => {
-                    const resMessage =
-                        (error.response &&
-                            error.response.data &&
-                            error.response.data.message) ||
-                        error.message ||
-                        error.toString();
-                    setMessage(resMessage);
-                    setSuccessful(false);
-                }
-            );
-        console.log("message ", message);
-    };
-    return (
-        <>
-            <>
-                {/* <div className="container">
+  
+  // console.log(arr)
+  var a = []
+  const onchangeInput = (e) => {
+    const val = e.split(',').map((a)=>{return (parseInt(a))}) ;
+    setDatas(val)
+    console.log("@@@",datas);
+  };
+  // setDatas()
+  
+  return (
+    <>
+      <>
+        {/* <div className="container">
                 <div className="upload_container">
                     <div className="upload_title_container">
                         <h1 className="upload_title">Edit Content</h1>
@@ -257,76 +272,84 @@ function UpdateMovie() {
                     )}
                 </div>
             </div> */}
-            </>
-            <>
-                <div className="container">
-                    <div className="updateMovie">
-                        <h1 className="formTitle">Update Movie</h1>
-                        <form action="" className="updateForm">
-                            <div className="movieInput">
-                                <label htmlFor="id">Movie Id</label>
-                                <input type="number" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_name">Movie Name</label>
-                                <input type="text" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_title">Movie Title</label>
-                                <input type="text" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_release">Release</label>
-                                <input type="date" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_popularity">
-                                    Popularity
-                                </label>
-                                <input type="number" step="0.1" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_budget">Budget</label>
-                                <input type="number" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_revenue">Revenue</label>
-                                <input type="number" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_poster">Poster URL</label>
-                                <input type="text" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_thumbnail">
-                                    Thumbnail URL
-                                </label>
-                                <input type="text" />
-                            </div>
-                            <div className="movieInput">
-                                <label htmlFor="movie_url">Movie URL</label>
-                                <input type="text" />
-                            </div>
-                            <div className="descp">
-                                <div className="movieInput description">
-                                    <label htmlFor="movie_descp">
-                                        Movie Description
-                                    </label>
-                                    <textarea type="text" />
-                                </div>
-                                <div className="movieInput description">
-                                    <label htmlFor="movie_descp">
-                                        Movie Description
-                                    </label>
-                                    <textarea type="text" />
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </>
-        </>
-    );
+      </>
+      <>
+        <div className="container">
+          <div className="updateMovie">
+            <h1 className="formTitle">Update Movie</h1>
+            <form action="" className="updateForm">
+              <div className="movieInput">
+                <label htmlFor="id">Movie Id</label>
+                <input type="number" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_name">Movie Name</label>
+                <input type="text" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_title">Movie Title</label>
+                <input type="text" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_release">Release</label>
+                <input type="date" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_popularity">
+                  Popularity
+                </label>
+                <input type="number" step="0.1" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_budget">Budget</label>
+                <input type="number" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_revenue">Revenue</label>
+                <input type="number" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_poster">Poster URL</label>
+                <input type="text" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_thumbnail">
+                  Thumbnail URL
+                </label>
+                <input type="text" />
+              </div>
+              <div className="movieInput">
+                <label htmlFor="movie_url">Movie URL</label>
+                <input type="text" />
+              </div>
+              {/* <div className="descp"> */}
+              <div className="movieInput description">
+                <label htmlFor="movie_descp">
+                  Movie Description
+                </label>
+                <textarea type="text" />
+              </div>
+              <div className="movieInput description">
+                <label htmlFor="movie_descp">
+                  Movie Description
+                </label>
+                <input type="text" onChange={(e)=> onchangeInput(e.target.value)}/>
+                {/* <Select
+                  defaultValue={[optionLength[2], optionLength[3]]}
+                  isMulti
+                  name="genres"
+                  options={optionLength}
+                  className="basic-multi-select"
+                  classNamePrefix="select"
+                /> */}
+              </div>
+              {/* </div> */}
+            </form>
+          </div>
+        </div>
+      </>
+    </>
+  );
 }
 
 export default UpdateMovie;
